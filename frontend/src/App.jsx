50a7452eaa53134b90099ab5789a30ae6c6ad523
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-12-15 16:32:44 +0500

    background and logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Router } from "./Router/Router";
 import { ConfigProvider, Flex, Layout, theme } from "antd";
 import { observer } from "mobx-react-lite";
 import { auth } from "./store/auth";
-import etagi_logo from "./asserts/etagi_logo.webp";
+// import etagi_logo from "./asserts/etagi_logo.webp";
 import { LogoutOutlined } from "@ant-design/icons";
 import { useEffect } from "react";
 import { Message } from "./components/UI/Message";
@@ -48,7 +48,11 @@ export const App = observer(() => {
                         }}
                     >
                         <div className="demo-logo">
-                            <img src={etagi_logo} alt="" />
+                            {/* <img src={etagi_logo} alt="" /> */}
+                            <img
+                                src="https://img.razrisyika.ru/kart/94/372802-logo-6.jpg"
+                                alt="Logo"
+                            />
                         </div>
                         {isAuth && (
                             <Flex>
