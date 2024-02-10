75be65bcecb874ff1ae101b41dfbeb092f8fefbd
Daniil Abdulov <abdulov.dany@yandex.ru>
2024-02-10 13:30:05 +0500

    some changes

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -7,7 +7,7 @@ export const development = {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    host: "db",
+    // host: "db",
   },
   pool: {
     min: 2,
